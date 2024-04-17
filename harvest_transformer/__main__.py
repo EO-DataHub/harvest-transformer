0f71ffbc2e40fa0281d9456c0c8182be8a44f3d4
Hannah Collingwood <hannah.collingwood@telespazio.com>
2024-04-17 14:34:37 +0100

    EODHP-275 - update variable name

--- harvest_transformer/__main__.py
+++ harvest_transformer/__main__.py
@@ -29,10 +29,10 @@ if os.getenv("AWS_ACCESS_KEY") and os.getenv("AWS_SECRET_ACCESS_KEY"):
         aws_access_key_id=os.environ["AWS_ACCESS_KEY"],
         aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
     )
-    s3 = session.client("s3")
+    s3_client = session.client("s3")
 
 else:
-    s3 = boto3.client("s3")
+    s3_client = boto3.client("s3")
 logging.basicConfig(level=logging.DEBUG, format="%(message)s")
 
 
